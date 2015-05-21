6753eb5c062473d50446e845c81d3e92ed098e8a
Rishi Gaurav Bhatnagar <rishibhatnagar1@gmail.com>
2015-05-21 17:02:44 +0530

    python module for esp

--- SandBox/usingExpress/app.js
+++ SandBox/usingExpress/app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app =express();
 
+/* This function will calculate and tell you if the temp is physical conditions are good */
+
+function physicalConditions(temp, pres){
+	if (temp > 30 || pres >10){
+		console.log("Too hot");
+			}
+	else {
+		console.log("Its fine bro");
+	}
+}
+
 app.get('/sensorValue',function(req,res){
 	var curDate = new Date();
-	var timeNow = curDate.getDate()+"/"+curDate.getHours()+"/"+curDate.getMinutes();
+	var timeNow = curDate.getDate()+"@"+curDate.getHours()+":"+curDate.getMinutes();
 	console.log("Temp: ",req.query.temp);
 	console.log("Pressure: ",req.query.pressure);
 	console.log("Time: ",timeNow);
+	//console.log(typeof temp);
+	physicalConditions(parseInt(req.query.temp),parseInt(req.query.pressure));
 });
 
 var server = app.listen(3000,function(){
